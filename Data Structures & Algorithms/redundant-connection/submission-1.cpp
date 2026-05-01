2d3be1c24e060456d56c71842182b7d580ce8802
Naveen Murthy <naveenk2k@gmail.com>
2026-04-30 21:45:27 -0500

    Add: redundant-connection - submission-1

---
+++ Data Structures & Algorithms/redundant-connection/submission-1.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<int> component, componentSize; 
+
+    int find(int a) {
+        while (a != component[a]) a = component[a];
+        return component[a];
+    }
+
+    void unite(int a, int b) {
+        a = find(a);
+        b = find(b);
+
+        // point larger component to smaller
+        if (componentSize[a] < componentSize[b]) swap(a, b);
+        component[a] = b;
+        componentSize[b] += componentSize[a];
+    }
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        int mx = 0;
+        for (vector<int> edge : edges) {
+            mx = max({mx, edge[0], edge[1]});
+        }
+
+        int n = mx+1;
+        component.resize(n);
+        componentSize.resize(n);
+
+        for (int i = 0; i < n; ++i) {
+            component[i] = i;
+            componentSize[i] = 1;
+        }
+
+        for (vector<int>& edge : edges) {
+            int a = edge[0];
+            int b = edge[1];
+            if (find(a) == find(b)) return edge;
+            unite(a, b);
+        }
+    }
+};
